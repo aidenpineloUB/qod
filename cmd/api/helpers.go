cc53c334e53359264402d1346444f9467496b07b
aidenpineloUB <123344652+aidenpineloUB@users.noreply.github.com>
2025-08-27 13:39:16 -0600

    commit message for v5

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,3 +1,4 @@
+// Filename: cmd/api/helpers.go
 package main
 
 import (
@@ -5,8 +6,10 @@ import (
   "net/http"
 )
 
+  // create an envelope type
+  type envelope map[string]any
 func (a *applicationDependencies)writeJSON(w http.ResponseWriter,
-                                           status int, data any,
+                                           status int, data envelope,
                                            headers http.Header) error  {
     jsResponse, err := json.MarshalIndent(data, "", "\t")
     if err != nil {
@@ -21,25 +24,26 @@ func (a *applicationDependencies)writeJSON(w http.ResponseWriter,
     w.Header().Set("Content-Type", "application/json")
     // explicitly set the response status code
     w.WriteHeader(status) 
-    _, err = w.Write(jsResponse)
+    _, err = w.Write(jsResponse) // 👈 This line is the fix
     if err != nil {
         return err
     }
 
     return nil
-
 }
 
-
-func (a *applicationDependencies)healthcheckHandler(w http.ResponseWriter,r *http.Request) {
-   data := map[string]string {
-                                "status": "available",
-                                "environment": a.config.environment,
-                                "version": appVersion,
-                              }
+func (a *applicationDependencies)healthcheckHandler(w http.ResponseWriter,
+                                               r *http.Request) {
+   data := envelope {
+                     "status": "available",
+                     "system_info": map[string]string{
+                             "environment": a.config.environment,
+                             "version": appVersion,
+                    },
+   }
    err := a.writeJSON(w, http.StatusOK, data, nil)
    if err != nil {
     a.logger.Error(err.Error())
-    http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+    http.Error(w, "The server encountered a problem and couldnot process your request", http.StatusInternalServerError)
    }
 }
