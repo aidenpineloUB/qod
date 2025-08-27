30fbd0108a3e00033480c74f02fede6d9dc3dba5
aidenpineloUB <123344652+aidenpineloUB@users.noreply.github.com>
2025-08-27 13:52:09 -0600

    final v7 commit

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,10 +9,17 @@ import (
 
 // routes specifies our routes (routes.go)
 func (app *applicationDependencies) routes() http.Handler {
+   // setup a new router
    router := httprouter.New()
+   // handle 404
+   router.NotFound = http.HandlerFunc(app.notFoundResponse)
+  // handle 405
+   router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+  
    router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 
    return router
 }
+
 
 
